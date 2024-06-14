837924be42c9e8cf50a7edb47b4efbcdbc050140
diydriller <dhrhd080@naver.com>
2024-06-15 00:14:50 +0900

    ADD daisyUI 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui"
 
 const config: Config = {
   content: [
@@ -10,6 +11,6 @@ const config: Config = {
     extend: {
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
 export default config;
