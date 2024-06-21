6c08da809de2d6ab9071bbcd51e326d082d389f2
inji0212 <in17100170@kakao.com>
2024-06-21 16:01:16 +0900

    FIX machine(테마 설정)페이지 퍼블리싱

--- app/machine/photo/page.tsx
+++ app/machine/photo/page.tsx
@@ -1,14 +1,38 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation"; // useRouter 대신 useRouter를 가져와야 함
 
 export default function Photo() {
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+  const router = useRouter();
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setUploadedImage(e.target?.result as string);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  const handleEditClick = () => {
+    if (uploadedImage) {
+      sessionStorage.setItem('uploadedImage', uploadedImage);
+      router.push('/machine/edit');
+    }
+  };
+
   return (
     <main className="px-20 pt-20 pb-[84px]">
       <h1>사진 전송하기</h1>
-      <Link href="/machine/edit" passHref>
-        <button className="bg-[#AE76CC] text-white h-8 w-20 rounded-[5px]">
+      <input type="file" onChange={handleImageUpload} />
+      {uploadedImage && (
+        <button onClick={handleEditClick} className="bg-[#AE76CC] text-white h-8 w-20 rounded-[5px]">
           편집하기
         </button>
-      </Link>
+      )}
     </main>
   );
 }
