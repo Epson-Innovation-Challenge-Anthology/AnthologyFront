5a93c1aa07ceda886bb24fd17700e26d739c2a5b
diydriller <dhrhd080@naver.com>
2024-06-21 22:53:37 +0900

    FIX 헤더 간격, 색 수정

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,7 +4,7 @@ import HeaderLoginButton from "./HeaderLoginButton";
 
 const Header: React.FC = () => {
   return (
-    <header className="flex items-center h-[164px] justify-between font-medium text-xl px-20 bg-[#C5B6DD] text-[#8600EF]">
+    <header className="flex items-center h-[77px] justify-between font-medium text-xl px-20 bg-[#925BB0] text-[#FFDDDD]">
       <div>
         <HeaderLink href="/">앤솔:로지</HeaderLink>
       </div>
